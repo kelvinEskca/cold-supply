3932edf1d2db36c8851705ff905f070ce5119396
kelvinEskca <beredugokelvin@gmail.com>
2023-01-10 15:32:17 +0100

    update

--- client/src/views/Soon.js
+++ client/src/views/Soon.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from "axios";
 const Soon = () => {
+    const [email,setEmail] = useState('');
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        if(email !== ""){
+            try{
+                const submit = await axios.post('http://localhost:5000/api/subscribe',{
+                    email:email
+                });
+                if(submit.status === 200){
+                    alert('Your Subscription is successful');
+                }
+            }
+            catch(err){
+                console.log(err);
+            }
+        }
+        else{
+            alert('Please Enter Your Email')
+        }
+    }
     return (
         <section className="section hero soon">
             <video autoPlay muted loop id="myVideo" src="../assets/video.mp4"></video>
@@ -10,8 +31,8 @@ const Soon = () => {
                         <h3 className="heading">Sign up for updates</h3>
                         <p className="paragraph"><b>COLDSUPPLY</b> isn't just another brand. We are the voice of the youths who strive for greatness and thrive on individuality not bound by self imposed limitations.</p>
 
-                        <form className="form" action="https://formsubmit.co/support@cold-supply.com" method="POST" >
-                            <label htmlFor="#"><input type="email" name="email" placeholder="Enter Email Address" /></label>
+                        <form className="form" onSubmit={handleSubmit} >
+                            <label htmlFor="#"><input type="email" placeholder="Enter Email Address" onChange={(e)=>setEmail(e.target.value)}/></label>
                             <label htmlFor="#"><button>Register</button></label>
                         </form>
                     </div>
